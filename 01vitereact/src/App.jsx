f9147b0f5c7cd7fb215f6266d543c3b432139878
Yashvigor <yashvigor.ce@gmail.com>
2026-01-22 19:29:56 +0530

    Initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+<h1>Chai aur react with Vite|| Yashvi Gor</h1>
+  )
+}
+
+export default App
