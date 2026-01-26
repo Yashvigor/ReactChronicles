5ecab8362be91313804aeafb06b7c8b439d4de1d
Yashvi Gor <yashvigor.ce@gmail.com>
2026-01-26 13:39:47 +0530

    Update main.jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+function MyApp(){
+  return (
+    <div>
+       <h1>Custom App</h1>
+    </div>
+  )
+}
+ReactDOMcreateRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyApp />
   </StrictMode>,
 )
