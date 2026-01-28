a220d71dd19e5c4d71b55909c0b3d5a6bcd515e5
Yashvi Gor <yashvigor.ce@gmail.com>
2026-01-28 18:53:41 +0530

    Update App.jsx

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,14 +8,15 @@ function App() {
   const [count, setCount] = useState(0)
   let myObj = {
     username: "yashvi",
-    age: 21
-  }
-  let newArr = [1, 2, 3] 
-
+    age: 19
+  } // the object can be passed 
+  let newArr = [1, 2, 3]  
+  //props is an object 
   return (
     <> //fragment
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card username="yashvi gor" btnText="click me" />
+      //props is an object so using that reference we can use its fields like username and age props.username, props.age
+      <Card username="yashvi gor" btnText="click me" /> //whatever is passed to this card is passed to props
       <Card username="mauli rami" />
     </>
   )
