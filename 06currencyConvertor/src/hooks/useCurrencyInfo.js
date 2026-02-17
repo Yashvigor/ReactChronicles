b1385012f53d6ebbfddc446a4ee6d87ec2f2f646
Yashvi Gor <yashvigor.ce@gmail.com>
2026-02-17 18:51:02 +0530

    Update useCurrencyInfo.js
    Added comments to clarify the purpose of state initialization and API response handling.

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -2,15 +2,15 @@ import {useEffect, useState} from "react"
 
 
 function useCurrencyInfo(currency){
-    const [data, setData] = useState({})
+    const [data, setData] = useState({}) // empty object if no value is returned or fetched from api
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
+        .then((res) => res.json()) // the response is converted to json 
+        .then((res) => setData(res[currency])) // data is hold 
         console.log(data);
-    }, [currency])
+    }, [currency]) // dependency array 
     console.log(data);
-    return data
+    return data 
 }
 
-export default useCurrencyInfo;
+export default useCurrencyInfo; 
