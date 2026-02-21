d63e66e98e7e9b2acce3834848afb2cb9159690b
Yashvi Gor <yashvigor.ce@gmail.com>
2026-02-21 10:03:04 +0530

    Update main.jsx

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom' 
+
+// Outlet: it uses layout as a base and wherever the outlet is used, the manipulation can be done there.
 
 function Layout() {
   return (
